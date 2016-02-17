a1af2c41903df49c7a70b2184d56931b92fbe214
Jonathan Wohl <jon@jonwohl.com>
2016-02-16 21:41:36 -0500

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Be VERY Careful. This script may be executed with admin privileges.
+
+echo "Openframe -- install.sh"
+
+# Some limited platform detection might be in order... though at present we're targeting the Pi
+os=$(uname)
+arq=$(uname -m)
+
+if [ $os == "Linux" ]; then
+
+    # on Debian Linux distributions
+
+    if [ $arq == "armv7l" ]; then
+        # on RaspberryPi
+        echo "armv7l"
+
+        # disable overscan
+        # sudo sed -i 's/.*overscan.*/#&/' /boot/config.txt
+
+        # rotate the display
+        # sudo echo "display_rotate=1" >> /boot/config.txt
+
+        # disable screen blanking
+        sudo sed -i -r 's/BLANK_TIME=[0-9]+/BLANK_TIME=0/' /etc/kbd/config
+        sudo sed -i -r 's/POWERDOWN_TIME=[0-9]+/POWERDOWN_TIME=0/' /etc/kbd/config
+
+        # sudo reboot
+    else
+        # Non-arm7 Debian...
+        echo "non armv7l"
+    fi
+
+elif [ $os == "Darwin" ]; then
+    # OSX
+    echo "osx"
+fi
