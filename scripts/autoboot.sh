0f3d2e86defd7560eb0e1d8dc7754cc0d485055b
Jeremias Volker <jeremias.volker@googlemail.com>
2019-09-23 03:52:24 +0200

    Fixes black window on SSH login
    Fixes #42

--- scripts/autoboot.sh
+++ scripts/autoboot.sh
@@ -6,10 +6,16 @@ read -t 1 -n 1 key
 then
     echo "no openframe"
 else
-    # hide the prompt:
-    PS1=""
-    # set the terminal text color to black
-    setterm --foreground black --background black --cursor off --clear all
-    # start up openframe as a background process
-    nohup openframe >/dev/null 2>&1 &
+    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+        echo "logged in via SSH"
+    else 
+        echo "logged in not via SSH"
+      
+        # hide the prompt:
+        PS1=""
+        # set the terminal text color to black
+        setterm --foreground black --background black --cursor off --clear all
+        # start up openframe as a background process
+        nohup openframe >/dev/null 2>&1 &
+    fi
 fi
